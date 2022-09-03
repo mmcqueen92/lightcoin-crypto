4002b84eda8a61e21dcab09067cf7f13a5d76206
Malcolm McQueen <malcolmmcqueen@live.com>
2022-09-03 01:39:08 +0000

    updated index.js code w/ Account class, made Transaction class which is extended by Withdrawal and Deposit classes

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
-let balance = 500.00;
+// let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
 
-  constructor(amount) {
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+
+}
+
+class Withdrawal extends Transaction  {
+
+  get value() {
+    return -this.amount;
   }
 
 }
 
 
 
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('Bingus');
+console.log(`Starting Balance: `, myAccount.balance);
+
+t1 = new Deposit(400, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log('Post-Deposit Balance:', myAccount.balance);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(50.25,myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
-
-console.log('Balance:', balance);
+console.log('Post-Withdrawal Balance:', myAccount.balance);
